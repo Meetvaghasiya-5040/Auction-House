c2163445e94d5280edf971ad58c6602b347aac81
Meetvaghasiya-5040 <145525480+Meetvaghasiya-5040@users.noreply.github.com>
2026-02-24 18:17:40 +0530

    profile

--- Home/models.py
+++ Home/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     profile_image = models.ImageField(
-        upload_to="profile_images/",
+        upload_to="media/profile_images/",
         blank=True,
         null=True,
         default="profile_images/default.png",
